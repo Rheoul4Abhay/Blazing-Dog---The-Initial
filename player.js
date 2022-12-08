72e9cd191d5b839f051ebf651c5087ba808d59e1
Abhay  Shrivastava <91346125+Rheoul4Abhay@users.noreply.github.com>
2022-12-08 20:37:11 +0530

    Update player.js

--- player.js
+++ player.js
@@ -2,7 +2,7 @@ import { StandingRight,StandingLeft,RunningRight,RunningLeft,SittingRight,Sittin
          
 export default class Player{
     constructor(gameWidth,gameHeight){
-        this.groundMargin = 65;
+        this.groundMargin = 5;
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
         this.image = document.getElementById('playerImage');
